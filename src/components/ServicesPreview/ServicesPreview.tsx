e22525b078223068141d2d1661d57fba51592774
safiuddinahmed <80223462+safiuddinahmed@users.noreply.github.com>
2025-09-27 18:42:46 -0400

    Consistent home page

--- src/components/ServicesPreview/ServicesPreview.tsx
+++ src/components/ServicesPreview/ServicesPreview.tsx
@@ -52,13 +52,14 @@ const ServicesPreview = () => {
   console.log('ServicesPreview component rendering with new design');
   return (
     <>
-      {/* Apple-Style Design - Consistent with HeroSection */}
-      <section className="py-32 bg-gray-50 overflow-hidden relative">
+      {/* Apple-Style Design - Distinct Background */}
+      <section className="py-32 bg-white overflow-hidden relative">
         
         {/* Background Pattern */}
         <div className="absolute inset-0">
-          <div className="absolute top-20 left-20 w-96 h-96 bg-red-500/10 rounded-full blur-3xl"></div>
-          <div className="absolute bottom-20 right-20 w-96 h-96 bg-red-600/10 rounded-full blur-3xl"></div>
+          <div className="absolute top-20 left-20 w-96 h-96 bg-red-500/5 rounded-full blur-3xl"></div>
+          <div className="absolute bottom-20 right-20 w-96 h-96 bg-red-600/5 rounded-full blur-3xl"></div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full bg-gradient-to-br from-red-50/30 via-transparent to-red-100/20"></div>
         </div>
 
         <div className="max-w-7xl mx-auto px-6 relative z-10">
